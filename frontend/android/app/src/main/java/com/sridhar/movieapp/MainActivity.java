290f3f1a4e800cfded95e0b6c873a6a60ce7b947
Sridhar <sridharsundar100@gmail.com>
2024-11-23 22:08:59 +0530

    android man

---
+++ frontend/android/app/src/main/java/com/sridhar/movieapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.sridhar.movieapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sridhar.movieapp',
+  appName: 'MovieTrackerApp',
+  webDir: 'build'
+};
+
+export default config;
